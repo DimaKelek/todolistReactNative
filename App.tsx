3ce8f36a988acd1fcf19d81fd39ab1dbf82a6897
Kelek <kelekofficial@gmail.com>
2021-10-15 15:54:14 +0300

    Changes: 1. adding tasks 2. validate errors in input

--- App.tsx
+++ App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {StyleSheet, Text, View} from 'react-native';
 import {AddItemForm} from "./src/components/Common/AddItemForm/AddItemForm";
 
+export type TaskType = {
+    id: string,
+    title: string
+}
+
 export const App: React.FC = () => {
+    const [tasks, setTasks] = useState<TaskType[]>([])
+
+    const taskItems = tasks.map(t => {
+        return (
+            <View key={t.id} style={styles.task}>
+                <Text>{t.title}</Text>
+            </View>
+        );
+    })
+
+    const addTask = (newTask: TaskType) => {
+        setTasks([newTask, ...tasks])
+    }
+
     return (
         <View style={styles.appWrapper}>
             <Text style={styles.text}>Hello Kelek</Text>
-            <AddItemForm />
+            <AddItemForm addTask={addTask}/>
+            {taskItems}
         </View>
     );
 };
@@ -23,5 +43,11 @@ const styles = StyleSheet.create({
         fontSize: 25,
         marginVertical: 20,
         textTransform: "uppercase"
+    },
+    task: {
+        width: "100%",
+        height: 50,
+        marginHorizontal: 10,
+        backgroundColor: "#fff"
     }
 })
--- src/components/Common/AddItemForm/AddItemForm.tsx
+++ src/components/Common/AddItemForm/AddItemForm.tsx
@@ -1,26 +1,55 @@
-import React from "react";
-import {Button, StyleSheet, TextInput, View} from "react-native";
+import React, {useState} from "react";
+import {Button, NativeSyntheticEvent, StyleSheet, Text, TextInput, TextInputChangeEventData, View} from "react-native";
+import {TaskType} from "../../../../App";
 
-export const AddItemForm: React.FC = props => {
+type AddItemFormProps = {
+    addTask: (newTask: TaskType) => void
+}
+
+export const AddItemForm: React.FC<AddItemFormProps> = ({addTask}) => {
+    const [value, setValue] = useState<string>("")
+    const [error, setError] = useState<string | null>(null)
+
+    const onChangeTitle = (e: NativeSyntheticEvent<TextInputChangeEventData>) => {
+        setValue(e.nativeEvent.text)
+        setError(null)
+    }
+
+    const onPressButton = () => {
+        if(!value) {
+            setError("Task title is required!")
+        } else if(value.length > 40){
+            setError("Task title shouldn't be more than 40 symbols")
+        } else {
+            addTask({id: "123", title: value})
+        }
+    }
+
     return (
-        <View style={styles.itemForm}>
-            <View style={styles.container}>
+        <View style={styles.wrapper}>
+            <View style={styles.form}>
                 <TextInput
-                    style={styles.input}
+                    style={[styles.input, {borderColor: error ? "#d41c50": "#0c0c0c"}]}
+                    placeholder="Task title ..."
+                    placeholderTextColor="#888383FF"
+                    value={value}
+                    onChange={onChangeTitle}
                 />
-                <Button title="Add"/>
+                <Button title="Add" disabled={!!error} onPress={onPressButton}/>
             </View>
+            {error && <Text style={styles.errorText}>{error}</Text>}
         </View>
     );
 }
 
 const styles = StyleSheet.create({
-    itemForm: {
+    wrapper: {
         width: "100%",
+        height: 90,
         paddingHorizontal: 10,
         //backgroundColor: "#a585c7"
     },
-    container: {
+    form: {
         flexDirection: "row",
         justifyContent: "space-between",
         alignItems: "center",
@@ -28,7 +57,17 @@ const styles = StyleSheet.create({
     },
     input: {
         width: "80%",
-        paddingHorizontal: 10,
-        backgroundColor: "#fff"
+        paddingHorizontal: 15,
+        backgroundColor: "#171616",
+        color: "#ffffff",
+        borderStyle: "solid",
+        borderWidth: 2,
+        borderRadius: 5
+    },
+
+    errorText: {
+        margin: 5,
+        color: "#d41c50",
+        fontSize: 18
     }
 })
